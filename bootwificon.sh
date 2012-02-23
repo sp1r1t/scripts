7ac0d1d94d0af72f5f8bc0879fe5fbf0af4609ae
Julian Konrath <sp1r1t@jinn.at>
2012-02-23 17:43:33 +0100

    shrinet added

--- bootwificon.sh
+++ bootwificon.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-sudo ifconfig wlan0 up
-string=$(sudo iwlist wlan0 scan | grep "red leader")
-if [ -n "$string" ]; then
+ifconfig wlan0 up
+string=$(iwlist wlan0 scan)
+wifi=$(echo $string  | grep "red leader")
+if [ -n "$wifi" ]; then
     echo "you're at home, baby"
-    sudo wpa_connecnt home
+    wpa_connect home
+fi
+wifi=$(echo $string  | grep "looong cat")
+if [ -n "$wifi" ]; then
+    echo "mifi tiem"
+    wpa_connect mifi
+fi
+wifi=$(echo $string  | grep "shaolin3")
+if [ -n "$wifi" ]; then
+    echo "amitufo"
+    wpa_connect sta3
 fi
