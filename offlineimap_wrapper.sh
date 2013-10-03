19c1ddec2d1e8f35fc1aa6a9aa3fe6eb0f47d53e
sp1r1t <office@jinn.at>
2013-10-03 17:36:18 +0200

    upd

--- offlineimap_wrapper.sh
+++ offlineimap_wrapper.sh
@@ -2,11 +2,10 @@
 
 # test internet connection
 inet=0
-if ping -W 3 -c 1 8.8.8.8 >/dev/null; then
+if ping -W 3 -c 1 8.8.8.8 &> /dev/null; then
     inet=1
-    #echo "connected to the interwebz"
-#else
-    #echo "no webz connection"
+else
+    exit
 fi
 
 if [ $inet -eq 1 ]; then
