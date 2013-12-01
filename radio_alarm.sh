8b44b7202500fa695ebc015fc8250a95d17fcd54
sp1r1t <office@jinn.at>
2013-12-01 23:30:33 +0100

    added time set to radio alarm

--- radio_alarm.sh
+++ radio_alarm.sh
@@ -1,56 +1,128 @@
 #!/bin/bash
 
+function usage {
+  echo "usage: ./`basename $0` <hours to sleep>"    
+}
+
+function naptime {
+  echo "it's now $hour:$minute"
+  echo "you can nap for $sleephours hours and $sleepminutes minutes, have a good rest"
+}
+
+# need arguments
 if [ $# -lt 1 ]; then
-    # print usage
-    echo "usage: ./`basename $0` <hours to sleep>"
-    exit
-else
-    # set sleep time
-    sleeptime=`echo "$1 * 60 * 60" | bc`
-    
-    # cut rational part
-    if [ `echo $sleeptime | grep "\."` ]; then
-        sleeptime=`echo $sleeptime | cut -d '.' -f1`
-    fi
+  usage
+  exit
 fi
 
-# current time
-hour=`date +%H`
-minute=`date +%M`
+# scan for options
+while getopts "ht:" opt; do
+  case "$opt" in
+    h)
+      usage
+      exit
+      ;;
+    t)
+      time=$OPTARG
+      if [ "`echo $time | sed -e '/^[0-9][0-9]:[0-9][0-9]$/d'`" != "" ]; then
+        usage
+        echo "the -t time format is HH:MM"
+        exit
+      fi
+      ;;
+    *) 
+      exit
+  esac
+done
 
-echo "it's now $hour:$minute"
 
-# convert input
-var=$1
-if [ `echo $var | grep "\."` ]; then
+if [ $time ]; then
+  # current time
+  hour=`date +%H`
+  minute=`date +%M`
+
+  # wake time
+  wakehour=`date -d "$time" "+%H"`
+  wakeminute=`date -d "$time" "+%M"`
+
+  wakehour=${wakehour#0}
+  wakeminute=${wakeminute#0}
+
+  # hour diff
+  if [ $hour -le $wakehour ]; then
+    sleephours=$(($wakehour - $hour))
+  else
+    sleephours=$(($wakehour + 24 - $hour))
+  fi    
+
+  # minute diff
+  if [ $minute -le $wakeminute ]; then
+    sleepminutes=$(($wakeminute - $minute))
+  else
+    sleepminutes=$(($wakeminute + 60 - $minute))
+    if [ $sleephours -eq 0 ]; then
+      sleephours=23
+    else
+      sleephours=$(($sleephours - 1))
+    fi
+  fi    
+
+  sleeptime=$(($sleephours * 60 * 60 + $sleepminutes * 60))
+else
+  # set sleep time
+  sleeptime=`echo "$1 * 60 * 60" | bc`
+  
+  # cut rational part
+  if [ `echo $sleeptime | grep "\."` ]; then
+    sleeptime=`echo $sleeptime | cut -d '.' -f1`
+  fi
+
+
+  # current time
+  hour=`date +%H`
+  minute=`date +%M`
+
+  # convert input
+  var=$1
+  if [ `echo $var | grep "\."` ]; then
     varInt=`echo $var | cut -d '.' -f1`
     varRest=`echo $var | cut -d '.' -f2`
     varFirstPlace=`echo $varRest | cut -c1`
-else
+  else
     varInt=$var
     varRest=0
     varFirstPlace=0
-fi
+  fi
 
 
-# calc new minutes
-if [ $varFirstPlace -eq 0 ]; then
+  # calc new minutes
+  if [ $varFirstPlace -eq 0 ]; then
     newminutes=0
-else
+  else
     newminutes=$((6 * $varFirstPlace))
-fi
+  fi
 
-# set wakeup time
-wakehour=$(($hour + $varInt))
-wakeminute=$(($minute + $newminutes))
+  # set wakeup time
+  wakehour=$(($hour + $varInt))
+  wakeminute=$(($minute + $newminutes))
 
-# check minute overflow in wakeuptime
-if [ $wakeminute -ge 60 ]; then
+  # check hour overflow in wakeuptime
+  if [ $wakehour -ge 24 ]; then
+    wakehour=$(($wakehour - 24))
+  fi
+
+  # check minute overflow in wakeuptime
+  if [ $wakeminute -ge 60 ]; then
     wakehour=$(($wakehour + 1))
     wakeminute=$(($wakeminute - 60))
+  fi
+
+  # set sleephours and mintues
+  sleephours=$(($sleeptime / 60 / 60))
+  sleepminutes=$((($sleeptime - $sleephours * 60 * 60) / 60))
 fi
 
+naptime
+printf "sleeping %d seconds until %02d:%02d\n" "$sleeptime" "$wakehour" "$wakeminute"
 
-printf "sleeping %d seconds until %2d:%02d\n" "$sleeptime" "$wakehour" "$wakeminute"
-
-sleep $sleeptime; iceweasel http://www.jazzradio.com/smoothlounge
+#sleep $sleeptime; iceweasel http://www.jazzradio.com/smoothlounge
